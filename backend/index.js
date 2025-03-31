1bbb800352527cebfb8a1e509dcc23ea66632068
d-rossi <david.rossi101@gmail.com>
2025-03-30 20:52:49 -0400

    get articles basic endpoint

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,14 @@ app.get('/', (request, response) => {
     response.send("<h1>HELLO WORLD from the conduit app!</h1>")
 })
 
+app.get('/articles', (request, response) => {
+    const article = {id: "1", title: "title", userId: "u1"}
+    //get from db
+
+    //return
+    response.json(article)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`)
