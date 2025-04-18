81ec085d28d2708bd8c5e2dd29e4ac2c5968a5e6
d-rossi <david.rossi101@gmail.com>
2025-04-18 15:31:01 -0400

    added routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,20 +5,26 @@ import Home from './pages/Home.jsx'
 import Register from './pages/Register.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
 import '../index.css'
+import ProtectedRoutes from './utils/ProtectedRoutes.jsx'
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
-    errorElement: <NotFoundPage />
+    element: <ProtectedRoutes />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+    ]
   },
   {
     path: '/signup',
-    element: <Register />
+    element: <Register isSignUp={true} />
   },
   {
     path: '/login',
-    element: <Register />
+    element: <Register isSignUp={false} />
   },
 ])
 
