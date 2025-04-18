9bb4194bff548daf3f633a82afbfbc103f943681
d-rossi <david.rossi101@gmail.com>
2025-04-18 13:06:49 -0400

    add routing and notFoundPage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Register from './pages/Register.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
+import '../index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <NotFoundPage />
+  },
+  {
+    path: '/signup',
+    element: <Register />
+  },
+  {
+    path: '/login',
+    element: <Register />
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
