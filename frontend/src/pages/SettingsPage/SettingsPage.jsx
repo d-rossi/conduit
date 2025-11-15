cbc87a35957e36ec7c41c8bbe9c6be6290b3528e
d-rossi <david.rossi101@gmail.com>
2025-11-15 16:36:37 -0500

    completed settings ui

--- frontend/src/pages/SettingsPage/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage/SettingsPage.jsx
@@ -1,16 +1,26 @@
+import { useEffect, useState } from "react";
 import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
 import TextArea from "../../components/TextArea/TextArea";
 import "./SettingsPage.css"
+import { getFollowers } from "../../services/followerService";
 
 const SettingsPage = () => {
+    const [showProfile, setShowProfile] = useState(true);
+    const [activeHeader, setActiveHeader] = useState("Following");
+    const [followers, setFollowers] = useState([]);
+
+    useEffect(() => {
+        setFollowers(getFollowers());
+    }, [])
 
     return (
         <div className="settings">
             <ul className="settings__nav">
-                <li>My Profile</li>
-                <li>Followers</li>
+                <li onClick={() => setShowProfile(true)} className={showProfile ? "settings__nav--active" : ""}>My Profile</li>
+                <li onClick={() => setShowProfile(false)} className={!showProfile ? "settings__nav--active" : ""}>Followers</li>
             </ul>
+            {showProfile && 
             <div className="settings__content">
                 <h3>Kate Scott</h3>
                 <img></img>
@@ -28,6 +38,48 @@ const SettingsPage = () => {
                     <Button text={"Delete Account"}/>
                 </div>
             </div>
+            }
+            {!showProfile &&
+            <div className="settings__content">
+                <table className="settings__content__table">
+                    <thead>
+                        <tr>
+                        <th onClick={() => setActiveHeader("Following")} className={activeHeader === "Following" ? "settings__content__table__header--active" : ""}>Following</th>
+                        <th onClick={() => setActiveHeader("Followers")} className={activeHeader === "Followers" ? "settings__content__table__header--active" : ""}>Followers</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>
+                            <div className="settings__content__table__data">
+                                <img src="https://cdn.guardian.ng/wp-content/uploads/2025/07/IMG_5947.jpeg" className="avatar"/>
+                                <span>Kate Scott</span>
+                            </div>
+                        </td>
+                        <td><Button text={"Unfollow"}/></td>
+                        </tr>
+                        <tr>
+                        <td>
+                            <div className="settings__content__table__data">
+                                <img src="https://via.placeholder.com/40" class="avatar"/>
+                                <span>Kate Scott</span>
+                            </div>
+                        </td>
+                        <td><Button text={"Unfollow"}/></td>
+                        </tr>
+                        <tr>
+                        <td>
+                            <div className="settings__content__table__data">
+                                <img src="https://via.placeholder.com/40" class="avatar"/>
+                                <span>Kate Scott</span>
+                            </div>
+                        </td>
+                        <td><Button text={"Unfollow"}/></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            }
         </div>
     )
 
