b0ca7012e1e4d4211d077790bb41ef79b843d308
d-rossi <david.rossi101@gmail.com>
2026-01-10 18:05:11 -0500

    add profile endpoints

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,6 +2,7 @@ const routeHandler = require('express').Router()
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const tokenExtractor = require('../utils/tokenExtractor')
 
 routeHandler.post('/signup', async (request, response) => {
     const {username, email, password} = request.body
@@ -22,6 +23,28 @@ routeHandler.post('/login', async (request, response) => {
 
     const token = jwt.sign({username: username, id: existingUser._id}, process.env.SECRET, {expiresIn: "0.25h"})
     response.status(200).json({token, username: username})
+})
+
+routeHandler.use(tokenExtractor.verifyToken)
+
+routeHandler.get('/', async (request, response) => {
+    const username = request.user.username
+    const existingUser = await getExistingUserByUsername(username)
+    if (!existingUser) {
+       return response.status(404).json({error: 'User not found!'})
+    }
+    return response.json(existingUser)
+})
+
+routeHandler.patch('/', async (request, response) => {
+    const username = request.user.username
+    const { email } = request.body;
+    const upatedUser = await User.findOneAndUpdate({ username: username}, { $set: { email } }, { new: true })
+    if (upatedUser) {
+        response.status(200).send(upatedUser)
+    } else {
+        response.status(400).send({err: "The user was not found or you are unauthorized to update it"})
+    }
 })
 
 const getExistingUserByUsernameOrEmail = async (username, email) => {
