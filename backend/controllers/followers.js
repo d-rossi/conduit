825be5d9a236cba0bda18f00b1ccfc9f96f6b100
d-rossi <david.rossi101@gmail.com>
2026-01-24 17:16:25 -0500

    add follower and following routes

--- backend/controllers/followers.js
+++ backend/controllers/followers.js
@@ -24,50 +24,5 @@ routeHandler.get('/', async (request, response) => {
     const existingFollowers = await Follower.find({ following: loggedInUserId }).populate('userId', 'username').sort({ createdAt: -1 }).skip((pageNum - 1) * limitNum).limit(parseInt(limitNum))
     return response.json(existingFollowers)
 })
-    
-routeHandler.post('/', async (request, response) => {
-    const {targetUserId} = request.body
-    const loggedInUserId = request.user.id
-
-    if (loggedInUserId === targetUserId) {
-        return response.status(400).json({ err: "You cannot follow yourself." })
-    }
-
-    const existingUser = await User.findById(targetUserId)
-    if (!existingUser) {
-        return response.status(404).json({ err: "User not found." })
-    }
-
-    const existingFollow = await Follower.findOne({ userId: loggedInUserId, following: targetUserId })
-
-    if (existingFollow) {
-        return response.status(409).json({ err: "You already follow this user." })
-    }
-
-    new Follower({userId: loggedInUserId, following: targetUserId}).save()
-
-    return response.sendStatus(201)
-})
-
-routeHandler.delete('/:id/followers', async (request, response) => {
-    const targetUserId = request.params.id
-    const loggedInUserId = request.user.id
-
-    if (loggedInUserId === targetUserId) {
-        return response.status(400).json({ err: "You cannot unfollow yourself." })
-    }
-
-    const existingUser = await User.findById(targetUserId)
-    if (!existingUser) {
-        return response.status(404).json({ err: "User not found." })
-    }
-
-    const { deletedCount } = await Follower.deleteOne({ userId: loggedInUserId, following: targetUserId })
-    
-    if (!deletedCount) {
-        return response.status(400).send({err: "You cannot unfollow someone that you are not following."})
-    } 
-    return response.sendStatus(204)
-})
 
 module.exports = routeHandler
